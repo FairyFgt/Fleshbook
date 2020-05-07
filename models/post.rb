9fa2ddfc06f9b70a2eaa2a3e1fadffb5f2b024b6
FairyFgt <Rasmusdobrosi@gmail.com>
2020-05-07 13:26:43 +0200

    a

--- models/post.rb
+++ models/post.rb
@@ -3,11 +3,25 @@ class Post
         @db = db
     end
 
+    #Public: Extract all data from posts where the post is specified
+    #
+    #Examples:
+    # get_content(4)
+    # # => [4,0,null, "text", title,14]
+    #
+    # Returns array in form as a post
     def get_content(id)
         @db.execute('SELECT * FROM posts
         WHERE posts.id = ?', id)[0]
     end
 
+    #Public: Extract all data from posts
+    #
+    #Examples:
+    # get_all()
+    # # => everything from posts
+    #
+    # Returns as array in form of posts
     def get_all()
         @db.execute('SELECT * FROM posts')
     end
